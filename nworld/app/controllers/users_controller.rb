d7f9b13f7993f94ab79f2e62561bc4f639ea4701
Christine P. Fornoles <cfornoles@gbox.adnu.edu.ph>
2017-03-05 19:28:03 +0800

    fix tabs

--- nworld/app/controllers/users_controller.rb
+++ nworld/app/controllers/users_controller.rb
@@ -1,49 +1,24 @@
 class UsersController < ApplicationController
 
-def index
+	def index
+		@product=Product.all
+	end
 
-@product=Product.all
+	def show
+		@product=Product.find(params[:id])
+	end
 
-end
+	def usershow
+		@user=User.all
+	end
 
-def show
-@product=Product.find(params[:id])
-
-end
-
-def usershow
-
-@user=User.all
-end
-
-
-	
-	
-	
-def update
+	def update
 		@product = Product.find(params[:id])
 		@product.update_attributes(product_params)
 		redirect_to "/products/#{@product.id}"
 	end
-	
-	
+
 	def product_params
 		params.require(:product).permit(:name, :srp, :retail_price, :distributor_price, :num_stock, :reserved, :img)
-	end	
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
+	end
 end
