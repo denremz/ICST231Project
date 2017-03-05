c67dbf9886d34a64b8ae2bdc234bf46c5661bab5
Christine P. Fornoles <cfornoles@gbox.adnu.edu.ph>
2017-03-05 17:30:54 +0800

    add username to users

--- nworld/db/schema.rb
+++ nworld/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228015837) do
+ActiveRecord::Schema.define(version: 20170305092947) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20170228015837) do
     t.date     "birthday"
     t.string   "marital"
     t.string   "address"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
