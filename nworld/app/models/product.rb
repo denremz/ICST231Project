48521cfb38fb028e72014f8900a3db7c4902b571
Christine P. Fornoles <cfornoles@gbox.adnu.edu.ph>
2017-03-05 13:03:14 +0800

    retain original variable name

--- nworld/app/models/product.rb
+++ nworld/app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ActiveRecord::Base
-	IMG_URL=["/assets/bubble.png","/assets/cloudcream.png","/assets/cushion.png", "/assets/eyegel.png", "ucream.png"]
+	IMG_SRC=["/assets/bubble.png","/assets/cloudcream.png","/assets/cushion.png", "/assets/eyegel.png", "ucream.png"]
 end
