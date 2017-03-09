ddb0f584d1887c33b082ae76d4ddb6a985ac0a3b
Christine P. Fornoles <cfornoles@gbox.adnu.edu.ph>
2017-03-10 06:38:18 +0800

    user colums predefined class

--- nworldr2/app/models/user.rb
+++ nworldr2/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+	TYPE_CLASS = ["Admin", "Retailer"]
+	SEX_CLASS = ["Male", "Female"]
 end
