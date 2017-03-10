18c4cae7462ea84463f3c15be5e85f2925b2273c
Christine P. Fornoles <cfornoles@gbox.adnu.edu.ph>
2017-03-10 20:06:52 +0800

    fix product image source

--- nworldr2/app/models/product.rb
+++ nworldr2/app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ActiveRecord::Base
-	IMG_SRC=["/assets/bubble.png","/assets/cloudcream.png","/assets/cushion.png", "/assets/eyegel.png", "/assets/ucream.png"]
+	IMG_SRC=["bubble.png","cloudcream.png","cushion.png", "eyegel.png", "ucream.png"]
 end
